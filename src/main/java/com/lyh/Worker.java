f821a324dae0b8971a4077b4d7b0f6c4795c939a
Richard Liang <152163506+fearlesslyh@users.noreply.github.com>
2025-02-28 17:18:41 +0800

    demo

--- src/main/java/com/lyh/Worker.java
+++ src/main/java/com/lyh/Worker.java
@@ -1,6 +1,13 @@
 package com.lyh;
 
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeoutException;
 
 /**
  * @author 梁懿豪
@@ -10,8 +17,40 @@ import com.rabbitmq.client.ConnectionFactory;
 public class Worker {
     private final static String TASK_QUEUE_NAME = "task_queue";
 
-    public static void main(String[] args) {
+    public static void main(String[] argv) throws IOException, TimeoutException {
         ConnectionFactory connectionFactory = new ConnectionFactory();
         connectionFactory.setHost("localhost");
+
+        Connection connection = connectionFactory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.queueDeclare(TASK_QUEUE_NAME, true, false, false, null);
+        System.out.println(" [*] 等待消息中. 退出按 CTRL+C");
+
+        channel.basicQos(1);
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
+            System.out.println(" [x] 接收到消息: '" + message + "'");
+            try {
+                doWork(message);
+            }finally {
+                System.out.println(" [x] 完成任务");
+                channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+            }
+        };
+
+        channel.basicConsume(TASK_QUEUE_NAME, false, deliverCallback, consumerTag ->{});
+    }
+
+    private static void doWork(String message) {
+        for (char ch : message.toCharArray()) {
+            if (ch == '.') {
+                try {
+                    Thread.sleep(1000);
+                }catch (InterruptedException ignored) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
     }
 }
