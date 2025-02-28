1c1807d6851acdf3f008f04e78057903e33f3438
Richard Liang <152163506+fearlesslyh@users.noreply.github.com>
2025-02-28 16:26:38 +0800

    demo

---
+++ src/main/java/com/lyh/Worker.java
@@ -0,0 +1,13 @@
+package com.lyh;
+
+/**
+ * @author 梁懿豪
+ * @version 1.0
+ * @Github https://github.com/fearlesslyh
+ */
+public class Worker {
+    private final static String TASK_QUEUE_NAME = "task_queue";
+    public static void main(String[] args) {
+
+    }
+}
