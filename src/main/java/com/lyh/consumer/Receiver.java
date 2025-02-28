9117bd1f52a685335da4916dcad88a152455664e
Richard Liang <152163506+fearlesslyh@users.noreply.github.com>
2025-02-28 21:50:37 +0800

    demo

--- src/main/java/com/lyh/consumer/Receiver.java
+++ src/main/java/com/lyh/consumer/Receiver.java
@@ -15,21 +15,32 @@ public class Receiver {
     private final static String Exchange_Name = "logs";
 
     public static void main(String[] args) throws IOException, TimeoutException {
+        // 创建连接工厂
         ConnectionFactory connectionFactory = new ConnectionFactory();
+        // 设置主机地址
         connectionFactory.setHost("localhost");
 
+        // 创建连接
         Connection connection = connectionFactory.newConnection();
+        // 创建通道
         Channel channel = connection.createChannel();
+        // 声明交换机
         channel.exchangeDeclare(Exchange_Name, BuiltinExchangeType.FANOUT);
+        // 声明队列，这里是建一个临时队列
         String queueName = channel.queueDeclare().getQueue();
-        channel.queueBind(queueName, Exchange_Name, "");
+        // 绑定队列到交换机
+        channel.queueBind(queueName, Exchange_Name, "severity");
         System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
 
+        // 定义回调函数
         DeliverCallback deliverCallback = (tag, delivery) -> {
+            // 获取消息内容
             String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
+            // 打印消息内容
             System.out.println(" [x] Received '" + message + "'");
         };
 
+        // 消费消息
         channel.basicConsume(queueName, true, deliverCallback, tag -> {
         });
     }
--- src/main/java/com/lyh/provider/EmitLog.java
+++ src/main/java/com/lyh/provider/EmitLog.java
@@ -16,15 +16,24 @@ public class EmitLog {
     private static final String EXCHANGE_NAME = "logs";
 
     public static void main(String[] argv) throws Exception {
+        // 创建连接工厂
         ConnectionFactory factory = new ConnectionFactory();
+        // 设置主机地址
         factory.setHost("localhost");
+        // 创建连接
         try (Connection connection = factory.newConnection();
+             // 创建通道
              Channel channel = connection.createChannel()) {
+            // 声明交换机
             channel.exchangeDeclare(EXCHANGE_NAME, "fanout");
 
+            // 如果没有传入参数，则默认发送"info: Hello World!"
             String message = argv.length < 1 ? "info: Hello World!" :
+                    // 否则将参数拼接成一个字符串
                     String.join(" ", argv);
+            // 打印发送的消息
             System.out.println(" [x] Sent '" + message + "'");
+            // 发送消息
             channel.basicPublish(EXCHANGE_NAME, "", null, message.getBytes(StandardCharsets.UTF_8));
 
         }
