a352536b8a093b0f27ce1aa7a1e3d0cbbaa96ab2
Richard Liang <152163506+fearlesslyh@users.noreply.github.com>
2025-03-02 17:04:58 +0800

    Update TopicConsumer.java

--- src/main/java/com/lyh/consumer/TopicConsumer.java
+++ src/main/java/com/lyh/consumer/TopicConsumer.java
@@ -15,11 +15,15 @@ public class TopicConsumer {
     private static final String QUEUE_NAME = "order_events";
 
     public static void main(String[] args) throws IOException, TimeoutException {
+        //创建连接工厂
         ConnectionFactory connectionFactory = new ConnectionFactory();
+        //设置主机地址
         connectionFactory.setHost("localhost");
 
         Channel channel;
+        //创建连接
         Connection connection = connectionFactory.newConnection();
+        //创建通道
         channel = connection.createChannel();
 
         //声明Topic类型的交换机和队列
@@ -28,9 +32,11 @@ public class TopicConsumer {
         //绑定通配符（匹配所有以order.开头的事件）
         channel.queueBind(QUEUE_NAME, EXCHANGE_NAME, "order.*");
 
+        //设置回调函数
         DeliverCallback callback = (tag, delivery) -> {
             System.out.println("收到订单事件： " + new String(delivery.getBody()));
         };
+        //开始消费消息
         channel.basicConsume(QUEUE_NAME, true, callback, consumerTag -> {
         });
         System.out.println("等待接受订单事件......");
