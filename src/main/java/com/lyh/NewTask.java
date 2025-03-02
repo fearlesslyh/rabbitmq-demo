2e018f9f94c3349b4306bd52217788a7f76e715d
Richard Liang <152163506+fearlesslyh@users.noreply.github.com>
2025-03-02 16:13:49 +0800

    demo

--- src/main/java/com/lyh/NewTask.java
+++ src/main/java/com/lyh/NewTask.java
@@ -16,17 +16,24 @@ public class NewTask {
     private final static String TASK_QUEUE_NAME = "task_queue";
 
     public static void main(String[] argv) throws Exception {
+        // 创建连接工厂
         ConnectionFactory connectionFactory = new ConnectionFactory();
+        // 设置主机地址
         connectionFactory.setHost("localhost");
 
+        // 使用try-with-resources语句，自动关闭连接和通道
         try (Connection connection = connectionFactory.newConnection();
              Channel channel = connection.createChannel()) {
 
+            // 声明队列
             channel.queueDeclare(TASK_QUEUE_NAME, true, false, false, null);
+            // 将命令行参数连接成一个字符串
             String message = String.join(" ", argv);
 
+            // 发布消息
             channel.basicPublish("", TASK_QUEUE_NAME, MessageProperties.PERSISTENT_TEXT_PLAIN, message.getBytes(StandardCharsets.UTF_8));
 
+            // 打印发送的消息
             System.out.println(" [x] Sent '" + message + "'");
         }
 
